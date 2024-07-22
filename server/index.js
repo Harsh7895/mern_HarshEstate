6010c587d24826181526c29c24875113530e4dac
Harsh <harshsik7890@gmail.com>
2024-07-22 21:53:35 +0530

    index updated

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://harsh-estate-mern.vercel.app/"
+    "https://harsh-estate-mern.vercel.app"
   );
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
