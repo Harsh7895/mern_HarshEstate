1039e7503079d93f52a01c11219d6ca157eb9bfc
Harsh <harshsik7890@gmail.com>
2024-07-24 16:54:09 +0530

    .

--- server/utils/verifyUser.js
+++ server/utils/verifyUser.js
@@ -3,10 +3,10 @@ import jwt from "jsonwebtoken";
 
 export const verifyUser = (req, res, next) => {
   const token = req.cookies.access_token;
+  console.log(token);
   if (!token) {
     return next(ErrorHandler(403, "Not authorized")); // Corrected typo in error message
   }
-
   try {
     const user = jwt.verify(token, process.env.JWT_SECRET);
     req.user = user;
